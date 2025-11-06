7c7cf279f29712dbea5df116f0d7c6d74856999d
vraj-verma <vermavraj77@gmail.com>
2025-11-06 23:44:39 +0530

    replace with docusaurus

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,15 @@
-import type {ReactNode} from 'react';
+import type { ReactNode } from 'react';
 import clsx from 'clsx';
 import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
-import HomepageFeatures from '@site/src/components/HomepageFeatures';
 import Heading from '@theme/Heading';
+import HomepageFeatures from '@site/src/components/HomepageFeatures';
 
 import styles from './index.module.css';
 
 function HomepageHeader() {
-  const {siteConfig} = useDocusaurusContext();
+  const { siteConfig } = useDocusaurusContext();
   return (
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className="container">
@@ -26,7 +26,7 @@ function HomepageHeader() {
           <Link
             className="button button--primary button--lg"
             to="https://app.stackly.in"
-            style={{marginLeft: '1rem'}}>
+            style={{ marginLeft: '1rem' }}>
             Open Stackly App →
           </Link>
         </div>
@@ -36,7 +36,7 @@ function HomepageHeader() {
 }
 
 export default function Home(): ReactNode {
-  const {siteConfig} = useDocusaurusContext();
+  const { siteConfig } = useDocusaurusContext();
   return (
     <Layout
       title={`${siteConfig.title}`}
